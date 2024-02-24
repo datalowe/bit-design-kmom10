ed07f34d5d654c1c09bb0b77045075a56e80a9e4
datalowe <datalowe@posteo.de>
2024-02-24 18:58:45 +0100

    improve header styling. fix routing links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
 import { FirstPageComponent } from './components/first-page/first-page.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
+import { HighlightsPageComponent } from './components/highlights-page/highlights-page.component';
 
-export const routes: Routes = [{ path: '', component: FirstPageComponent }];
+export const routes: Routes = [
+  { path: '', component: FirstPageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'highlights', component: HighlightsPageComponent },
+];
